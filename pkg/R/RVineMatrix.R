5f8db6432c16e68d67dfca5ac9542e6adda9d62d
ulf <ulf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-12 14:57:46 +0000

    RVineMatrix: Funktion zur Überprüfung auf gültige Vine-Matrix (von Harry Joe) BiCop: neue Copula, reflection asymmetric copula (ebenfalls von Harry)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vinecopula@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RVineMatrix.R
+++ pkg/R/RVineMatrix.R
@@ -17,41 +17,45 @@ RVineMatrix  = function(Matrix,family=array(0,dim=dim(Matrix)),par=array(NA,dim=
 	if(any(!(family %in% c(0,1:10,13,14,16:20,23,24,26:30,33,34,36:40,43,44)))) stop("Copula family not implemented.")
 	if(length(names)>0 & length(names)!=dim(Matrix)[1]) stop("Length of the vector 'names' is not correct.")
 	
+	if(varraycheck(Matrix)!=1) stop("'Matrix' is not a valid R-vine matrix")
+	
 	if(!all(par %in% c(0,NA)))
 	{
-	for(i in 2:dim(Matrix)[1]){
-	for(j in 1:(i-1)){
-    	if((family[i,j]==1 || family[i,j]==2) && abs(par[i,j])>=1) stop("The parameter of the Gaussian and t-copula has to be in the interval (-1,1).")
-	if(family[i,j]==2 && par2[i,j]<=2) stop("The degrees of freedom parameter of the t-copula has to be larger than 2.")    	
-	if((family[i,j]==3 || family[i,j]==13) && par[i,j]<=0) stop("The parameter of the Clayton copula has to be positive.")
-    	if((family[i,j]==4 || family[i,j]==14) && par[i,j]<1) stop("The parameter of the Gumbel copula has to be in the interval [1,oo).")
-    	if((family[i,j]==6 || family[i,j]==16) && par[i,j]<=1) stop("The parameter of the Joe copula has to be in the interval (1,oo).")	
-    	if(family[i,j]==5 && par[i,j]==0) stop("The parameter of the Frank copula has to be unequal to 0.")
-    	if((family[i,j]==7 || family[i,j]==17) && par[i,j]<=0) stop("The first parameter of the BB1 copula has to be positive.")
-    	if((family[i,j]==7 || family[i,j]==17) && par2[i,j]<1) stop("The second parameter of the BB1 copula has to be in the interval [1,oo).")
-	if((family[i,j]==8 || family[i,j]==18) && par[i,j]<=0) stop("The first parameter of the BB6 copula has to be in the interval [1,oo).")
-	if((family[i,j]==8 || family[i,j]==18) && par2[i,j]<1) stop("The second parameter of the BB6 copula has to be in the interval [1,oo).")
-    	if((family[i,j]==9 || family[i,j]==19) && par[i,j]<1) stop("The first parameter of the BB7 copula has to be in the interval [1,oo).")
-    	if((family[i,j]==9 || family[i,j]==19) && par2[i,j]<=0) stop("The second parameter of the BB7 copula has to be positive.")
-	if((family[i,j]==10 || family[i,j]==20) && par[i,j]<1) stop("The first parameter of the BB8 copula has to be in the interval [1,oo).")
-	if((family[i,j]==10 || family[i,j]==20) && (par2[i,j]<=0 || par2[i,j]>1)) stop("The second parameter of the BB8 copula has to be in the interval (0,1].")    	
-        if((family[i,j]==23 || family[i,j]==33) && par[i,j]>=0) stop("The parameter of the rotated Clayton copula has to be negative.")
-    	if((family[i,j]==24 || family[i,j]==34) && par[i,j]>-1) stop("The parameter of the rotated Gumbel copula has to be in the interval (-oo,-1].")
-    	if((family[i,j]==26 || family[i,j]==36) && par[i,j]>=-1) stop("The parameter of the rotated Joe copula has to be in the interval (-oo,-1).")
-	if((family[i,j]==27 || family[i,j]==37) && par[i,j]>=0) stop("The first parameter of the rotated BB1 copula has to be negative.")
-	if((family[i,j]==27 || family[i,j]==37) && par2[i,j]>-1) stop("The second parameter of the rotated BB1 copula has to be in the interval (-oo,-1].")
-	if((family[i,j]==28 || family[i,j]==38) && par[i,j]>=0) stop("The first parameter of the rotated BB6 copula has to be in the interval (-oo,-1].")
-	if((family[i,j]==28 || family[i,j]==38) && par2[i,j]>-1) stop("The second parameter of the rotated BB6 copula has to be in the interval (-oo,-1].")
-	if((family[i,j]==29 || family[i,j]==39) && par[i,j]>-1) stop("The first parameter of the rotated BB7 copula has to be in the interval (-oo,-1].")
-	if((family[i,j]==29 || family[i,j]==39) && par2[i,j]>=0) stop("The second parameter of the rotated BB7 copula has to be negative.")
-	if((family[i,j]==30 || family[i,j]==40) && par[i,j]>-1) stop("The first parameter of the rotated BB8 copula has to be in the interval (-oo,-1].")
-	if((family[i,j]==30 || family[i,j]==40) && (par2[i,j]>=0 || par2[i,j]<(-1))) stop("The second parameter of the rotated BB8 copula has to be in the interval [-1,0).")
-  	  }
-	  }
+		for(i in 2:dim(Matrix)[1])
+		{
+			for(j in 1:(i-1))
+			{
+				if((family[i,j]==1 || family[i,j]==2) && abs(par[i,j])>=1) stop("The parameter of the Gaussian and t-copula has to be in the interval (-1,1).")
+				if(family[i,j]==2 && par2[i,j]<=2) stop("The degrees of freedom parameter of the t-copula has to be larger than 2.")    	
+				if((family[i,j]==3 || family[i,j]==13) && par[i,j]<=0) stop("The parameter of the Clayton copula has to be positive.")
+				if((family[i,j]==4 || family[i,j]==14) && par[i,j]<1) stop("The parameter of the Gumbel copula has to be in the interval [1,oo).")
+				if((family[i,j]==6 || family[i,j]==16) && par[i,j]<=1) stop("The parameter of the Joe copula has to be in the interval (1,oo).")	
+				if(family[i,j]==5 && par[i,j]==0) stop("The parameter of the Frank copula has to be unequal to 0.")
+				if((family[i,j]==7 || family[i,j]==17) && par[i,j]<=0) stop("The first parameter of the BB1 copula has to be positive.")
+				if((family[i,j]==7 || family[i,j]==17) && par2[i,j]<1) stop("The second parameter of the BB1 copula has to be in the interval [1,oo).")
+				if((family[i,j]==8 || family[i,j]==18) && par[i,j]<=0) stop("The first parameter of the BB6 copula has to be in the interval [1,oo).")
+				if((family[i,j]==8 || family[i,j]==18) && par2[i,j]<1) stop("The second parameter of the BB6 copula has to be in the interval [1,oo).")
+				if((family[i,j]==9 || family[i,j]==19) && par[i,j]<1) stop("The first parameter of the BB7 copula has to be in the interval [1,oo).")
+				if((family[i,j]==9 || family[i,j]==19) && par2[i,j]<=0) stop("The second parameter of the BB7 copula has to be positive.")
+				if((family[i,j]==10 || family[i,j]==20) && par[i,j]<1) stop("The first parameter of the BB8 copula has to be in the interval [1,oo).")
+				if((family[i,j]==10 || family[i,j]==20) && (par2[i,j]<=0 || par2[i,j]>1)) stop("The second parameter of the BB8 copula has to be in the interval (0,1].")    	
+				if((family[i,j]==23 || family[i,j]==33) && par[i,j]>=0) stop("The parameter of the rotated Clayton copula has to be negative.")
+				if((family[i,j]==24 || family[i,j]==34) && par[i,j]>-1) stop("The parameter of the rotated Gumbel copula has to be in the interval (-oo,-1].")
+				if((family[i,j]==26 || family[i,j]==36) && par[i,j]>=-1) stop("The parameter of the rotated Joe copula has to be in the interval (-oo,-1).")
+				if((family[i,j]==27 || family[i,j]==37) && par[i,j]>=0) stop("The first parameter of the rotated BB1 copula has to be negative.")
+				if((family[i,j]==27 || family[i,j]==37) && par2[i,j]>-1) stop("The second parameter of the rotated BB1 copula has to be in the interval (-oo,-1].")
+				if((family[i,j]==28 || family[i,j]==38) && par[i,j]>=0) stop("The first parameter of the rotated BB6 copula has to be in the interval (-oo,-1].")
+				if((family[i,j]==28 || family[i,j]==38) && par2[i,j]>-1) stop("The second parameter of the rotated BB6 copula has to be in the interval (-oo,-1].")
+				if((family[i,j]==29 || family[i,j]==39) && par[i,j]>-1) stop("The first parameter of the rotated BB7 copula has to be in the interval (-oo,-1].")
+				if((family[i,j]==29 || family[i,j]==39) && par2[i,j]>=0) stop("The second parameter of the rotated BB7 copula has to be negative.")
+				if((family[i,j]==30 || family[i,j]==40) && par[i,j]>-1) stop("The first parameter of the rotated BB8 copula has to be in the interval (-oo,-1].")
+				if((family[i,j]==30 || family[i,j]==40) && (par2[i,j]>=0 || par2[i,j]<(-1))) stop("The second parameter of the rotated BB8 copula has to be in the interval [-1,0).")
+			}
+		}
 	}
 
-  MaxMat=createMaxMat(Matrix)
-  CondDistr=neededCondDistr(Matrix)
+	MaxMat=createMaxMat(Matrix)
+	CondDistr=neededCondDistr(Matrix)
 					
 	RVM = list(Matrix = Matrix, family = family, par = par, par2 = par2, names = names, MaxMat = MaxMat, CondDistr = CondDistr)
 			   
@@ -229,4 +233,147 @@ as.RVineMatrix = function(RVine){
 	
 	return(RVineMatrix(M, family = Type, par = Param, par2 = Params2, names = names))
 	
+}
+
+
+###########################################################################
+# Code from Harry Joe (Thanks for that)
+
+
+# varray2NO:  vine array to natural order 
+# irev=F means A1[d,d]=A[d,d]
+# irev=T means A1[d,d]=A[d-1,d] (this option used to check if A is in
+#                   equivalence class of size 1 or 2).
+# A is a vine array; 1:d on diagonal is not necessary
+varray2NO=function(A,irev=FALSE,iprint=FALSE)
+{ 
+	d=nrow(A); d2=d-2; d1=d-1
+	A1=matrix(0,d,d)
+	T=vpartner(A)
+	if(irev) { A1[d,d]=A[d1,d] } else { A1[d,d]=A[d,d] }
+	for(k in d:2)
+	{
+		x=A1[k,k]
+		for(ell in 1:(k-1)) A1[ell,k]=which(T[x,]==ell)
+		T[x,]=0; T[,x]=0
+		A1[k-1,k-1]=A1[k-1,k]
+	}
+	# A1 satisfies A[i,i]=A[i,i+1]
+	if(iprint) print(A1)
+	# now apply permutation
+	iorder=order(diag(A1))
+	A2=A1
+	for(i in 1:d)
+	{ 
+		for(j in i:d) A2[i,j]=iorder[A1[i,j]] 
+	}
+	if(iprint) print(A2)
+	list(NOa=A1,NO=A2,perm=iorder,diag=diag(A1))
+}
+
+# This function is not in NAMESPACE (not for direct use).
+# Function with T(x,y)=ell if x-y are conditioned partners in tree l
+vpartner=function(A)
+{ d=nrow(A); 
+  T=matrix(0,d,d) 
+  for(j in 2:d)
+  { x=A[j,j]
+    for(k in 1:(j-1))
+    { y=A[k,j]; T[x,y]=k; T[y,x]=k }
+  }
+  T
+}
+
+# Check whether A in natural order is a valid vine array if it has
+#  permutation of 1:d on diagonal and permutation of 1;j in A[1:j,j]
+# Natural order also means A[j-1,j]=j-1 and A[j,j]=j
+# Function with A vine array (assumed dxd) and calls to vinvstepb
+# if(b==0) on input,
+#    columns 4 to d, binary elements of b are randomly generated 
+# output is b matrix with NA in lower triangle if A is OK
+#   otherwise returns -1
+#varraycheck=function(A)
+varray2bin=function(A)
+{ d=nrow(A)  
+  b=matrix(NA,d,d)
+  b[1,]=1
+  diag(b)=1
+  for(i in 3:d) b[i-1,i]=1
+  for(i in 4:d) 
+  { b0=vinvstepb(A,i)
+    #print(b0)
+    if(min(b0)==-1) return(-1)
+    b[1:i,i]=b0
+  }
+  b
+}
+
+
+# inverse for column i: 
+# input A has dimension at least ixi
+# output b has length i 
+# This function is not in NAMESPACE (not for direct use);
+# it is used by varraycheck
+vinvstepb=function(A,i,ichk0=0)
+{ # do these basic checks first
+  if(ichk0>0)
+  { diagA=diag(A[1:i,1:i])
+    if(max(diagA-(1:i))!=0) return(-1)
+    for(k in 2:i) { if(A[k-1,k]!=k-1) return(-1) }
+    if(A[1][3]!=1) return(-1);
+  }
+
+  b=rep(1,i)
+  itaken=rep(0,i)
+  itaken[i]=1
+  itaken[i-1]=1
+  ac=i-2  # active column
+  for(k in (i-2):1)
+  { if(A[k,i]==A[ac,ac])
+    { b[k]=1; 
+      tem=A[ac,ac]; itaken[tem]=1
+      if(k>1) { ac=max((1:i)[itaken==0]) }
+    }
+    else if(A[k,i]==A[k-1,ac])
+    { b[k]=0;
+      tem=A[k-1,ac]; itaken[tem]=1 
+    }
+    else return(-1);   # not valid A in NO(i)
+  }
+  b
+}
+
+
+
+# various checks for validity of a vine array A with dimension d>=4
+# return 1 for OK
+# return -3 for diagonal not 1:d
+# return -2 for not permutation of 1:j in column j
+# return -1 if cannot find proper binary array from array in natural order
+varraycheck=function(A)
+{
+	d=nrow(A)
+	if(d!=ncol(A)) return(-1)
+	
+	A=A[d:1,d:1]	# unsere Notation <-> Harrys Notation
+	
+	if(sum(abs(sort(diag(A))-(1:d)))!=0) return(-3)
+	# convert to 1:d on diagonal
+	iorder=order(diag(A))
+	A2=A
+	for(i in 1:d)
+	{ 
+		for(j in i:d) A2[i,j]=iorder[A[i,j]] 
+	}
+	#print(A2)
+	for(j in 2:d)
+	{ 
+		if(sum(abs(sort(A2[1:(j-1),j])-(1:(j-1))))!=0) return(-2) 
+	}
+	# next convert to natural order for more checks
+	if(d<=3) return(1)
+	ANOobj=varray2NO(A2)
+	#print(ANOobj)
+	b=varray2bin(ANOobj$NO) # if OK, a binary matrix is returned here
+	if(is.matrix(b)) return(1) else return(-1)
 }
